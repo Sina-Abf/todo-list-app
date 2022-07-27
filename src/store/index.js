0d5b97d07296f33a4313e93cdfe475842368c6ca
Sina-Abf <abf.sina@gmail.com>
2022-07-27 11:59:27 +0430

    todoSlice logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import todoSlice from './todo-slice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    todo: todoSlice.reducer,
+  },
 });
 
 export default store;
--- src/store/todo-slice.js
+++ src/store/todo-slice.js
@@ -1,1 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+  name: 'TODO',
+  initialState: { todoList: [] },
+  reducers: {
+    addNewTodo() {},
+    removeTodo() {},
+    updateTodo() {},
+    editTodo() {},
+  },
+});
+
+export const todoActions = todoSlice.actions;
+
+export default todoSlice;
